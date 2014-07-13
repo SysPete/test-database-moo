0417f6827410382cf7b3526b20726dd9c3ee6844
Peter Mottram <peter@sysnix.com>
2014-07-13 17:07:53 +0200

    initial commit

---
+++ lib/Test/Database/Moo.pm
@@ -0,0 +1,48 @@
+package Test::Database::Moo;
+
+use 5.006;
+use strict;
+use warnings FATAL => 'all';
+
+=head1 NAME
+
+Test::Database::Moo - The great new Test::Database::Moo!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Extends Test::Roo to simplify database testing on multiple backends.
+
+=head1 AUTHOR
+
+Peter Mottram (SysPete), C<< <peter at sysnix.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-test-moo-database at rt.cpan.org>, or through the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Test-Moo-Database>.  I will be notified, and then you'll automatically be notified of progress on your bug as I make changes.
+
+=head1 SUPPORT
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Peter Mottram (SysPete).
+
+This program is free software; you can redistribute under the same terms as Perl itself.
+
+See L<http://dev.perl.org/licenses/> for more information.
+
+=cut
+
+1;
